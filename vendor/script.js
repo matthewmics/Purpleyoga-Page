fef1ace4fd6b7bc85a181adbbc69139ab2655081
Matthew Miclat <38750461+matthewmics@users.noreply.github.com>
2021-09-09 19:16:21 +0800

    update

--- vendor/script.js
+++ vendor/script.js
@@ -1,34 +1,8 @@
 let navlogo;
-let container;
-
-function handleResize() {
-  const w = window.innerWidth;
-  if (window.scrollY > 90) {    
-    navlogo.classList.remove("logo-tablet", "logo-desktop");
-    navlogo.classList.add("logo-nav");
-    container.style.paddingTop = "0px";
-  } else if (w > 1200) {
-    navlogo.classList.remove("logo-nav", "logo-tablet");
-    navlogo.classList.add("logo-desktop");
-    container.style.paddingTop = "0px";
-  } else {
-    navlogo.classList.remove("logo-nav", "logo-desktop");
-    navlogo.classList.add("logo-tablet");
-    container.style.paddingTop = "30px";
-  }
-}
 
 (function () {
   navlogo = document.getElementById("logo");
-  container = document.querySelector("main > *:first-child");
-  handleResize();
-
-  window.addEventListener("scroll", () => {
-    handleResize();
-  });
-  window.addEventListener("resize", () => {
-    handleResize();
-  });
-
-  navlogo.classList.remove("hide");
+  navlogo.addEventListener("click", () => {
+    window.location.href = "index.html";
+  })
 })();
