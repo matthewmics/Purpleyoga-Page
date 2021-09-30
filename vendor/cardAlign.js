fbc38ae02cd0d941f0576db44978c82bb96d9e05
Matthew Miclat <38750461+matthewmics@users.noreply.github.com>
2021-09-30 11:20:20 +0800

    Update cardAlign.js

--- vendor/cardAlign.js
+++ vendor/cardAlign.js
@@ -37,8 +37,8 @@ function alignCard(row, isMob) {
   }
 }
 
-setTimeout(() => {
-  (function () {
+(function () {
+  waitAllImages(document.querySelector(".container"), function () {
     var rows = document.querySelectorAll(".row");
     rows.forEach((row) => {
       if (!row.classList.contains("noAlign"))
@@ -51,5 +51,57 @@ setTimeout(() => {
           alignCard(row, window.innerWidth <= 600);
       });
     });
-  })();
-}, 500);
+  });
+})();
+
+function waitAllImages(element, callback) {
+  var allImgsLength = 0;
+  var allImgsLoaded = 0;
+  var allImgs = [];
+
+  var filtered = Array.prototype.filter.call(
+    element.querySelectorAll("img"),
+    function (item) {
+      if (item.src === "") {
+        return false;
+      }
+
+      // Firefox's `complete` property will always be `true` even if the image has not been downloaded.
+      // Doing it this way works in Firefox.
+      var img = new Image();
+      img.src = item.src;
+      return !img.complete;
+    }
+  );
+
+  filtered.forEach(function (item) {
+    allImgs.push({
+      src: item.src,
+      element: item,
+    });
+  });
+
+  allImgsLength = allImgs.length;
+  allImgsLoaded = 0;
+
+  // If no images found, don't bother.
+  if (allImgsLength === 0) {
+    callback.call(element);
+  }
+
+  allImgs.forEach(function (img) {
+    var image = new Image();
+
+    // Handle the image loading and error with the same callback.
+    image.addEventListener("load", function () {
+      allImgsLoaded++;
+
+      if (allImgsLoaded === allImgsLength) {
+        callback.call(element);
+        return false;
+      }
+    });
+
+    image.src = img.src;
+  });
+}
